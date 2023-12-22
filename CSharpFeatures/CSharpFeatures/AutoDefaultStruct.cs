cf47856ab28be31d49f2b201c19c7c7163794b35
RupaMistry <mistryrupa7@gmail.com>
2023-12-23 01:26:04 +0400

    Added code snippets for Auto default struct feature.

---
+++ CSharpFeatures/CSharpFeatures/AutoDefaultStruct.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpFeatures
+{
+    public struct GeoCoordinate
+    {
+        private int _latitude;
+        public int Latitude
+        {
+            get => _latitude;
+            set => _latitude = value >= 0 ? value : throw new ArgumentOutOfRangeException();
+        }
+
+        public GeoCoordinate(int latitude)
+        {
+            /* The compiler will automatically insert below line
+             * ensuring that all fields are definitely assigned before returning or use */
+
+            // _latitude = default;
+        }
+    }
+}
